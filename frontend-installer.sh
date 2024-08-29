ef984aafbb8429e2e57d4ae38adb0c9dbe0e7b10
Alejandro Ordoñez <m.alejandro.ordonez@outlook.com>
2024-08-28 19:54:31 -0500

    Updates installer

--- frontend-installer.sh
+++ frontend-installer.sh
@@ -68,6 +68,17 @@ fi
 
 ############### Run the conatiner #############
 
+CONTAINER_NAME=bilan-web-container
+
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "The container '$CONTAINER_NAME' is running. Stopping it."
+    docker stop $CONTAINER_NAME
+else
+    echo "The container '$CONTAINER_NAME' is not running."
+fi
+
+echo ""
+
 # Clean any previous installation
 echo "Cleaning any previous containers and images..."
 docker prune images -a
@@ -77,19 +88,28 @@ docker system prune -a
 docker build -t bilan/front --build-arg TARGET=$TARGET .
 
 # Launches the container and serves in port 80
-docker run -d --name=bilan-web-container -p 80:80 bilan/front
+docker run -d --name=$CONTAINER_NAME -p 80:80 bilan/front
 
 ###############################################
 
 
 ############ Configure Docker service #########
+echo "Configuring service"
+echo ""
 SERVICE_NAME="bilan-web.service"
 SERVICE_PATH="/etc/systemd/system/$SERVICE_NAME"
-if check_file_exists $SERVICE_PATH; then
-    echo "The service is already created"
-else
-    cp ./resources/$SERVICE_NAME /etc/systemd/system/
+
+# Check if the service is enabled
+if systemctl is-enabled --quiet "$SERVICE_NAME"; then
+    echo "The service '$SERVICE_NAME' is enabled."
+    systemctl disable $SERVICE_NAME
 fi
+
+if check_file_exists $SERVICE_PATH; then
+    echo "The service is already created, copying latest version"
+fi
+
+cp ./resources/$SERVICE_NAME /etc/systemd/system/
 
 systemctl enable $SERVICE_NAME
 
