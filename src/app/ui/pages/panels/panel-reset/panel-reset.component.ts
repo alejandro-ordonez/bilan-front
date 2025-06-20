d1c17e0f978e8b2a9db2968225f600f485d22548
lichblitz <carloscortespa@gmail.com>
2025-06-19 21:52:04 -0500

    feat: ajustar  nombre reportes

--- src/app/ui/pages/panels/panel-reset/panel-reset.component.ts
+++ src/app/ui/pages/panels/panel-reset/panel-reset.component.ts
@@ -62,7 +62,7 @@ export class PanelResetComponent implements OnInit {
             const a = document.createElement('a');
             a.style.display = 'none';
             a.href = url;
-            a.download = cycleId + fileName + Date.now();
+            a.download = fileName
 
             document.body.appendChild(a);
             a.click();
